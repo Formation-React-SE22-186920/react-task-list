a7e5104d36d8c3f40fdebd3cea524e26de9c66b6
Moonpearl <romain.tello@gmail.com>
2022-02-15 14:48:44 +0100

    Add task modification

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,85 @@
-import { FormEventHandler, useEffect, useState } from 'react';
+import { FC, FormEventHandler, useEffect, useState } from 'react';
 import { Button, Container, Form, ListGroup } from 'react-bootstrap';
-import { Task } from './types/api';
-import { FaTrashAlt } from 'react-icons/fa';
+import { Task, TaskInput } from './types/api';
+import { FaCheck, FaPen, FaTimes, FaTrashAlt } from 'react-icons/fa';
+
+interface TaskListItemProps {
+  task: Task
+  deleteTask: (id: number) => void
+  updateTask: (id: number, updatedTask: TaskInput) => void
+}
+
+const TaskListItem: FC<TaskListItemProps> = ({ task, deleteTask, updateTask }) => {
+  // État permettant de savoir si l'élément est actuellement en cours de modification
+  const [isBeingEdited, setIsBeingEdited] = useState(false);
+  // État décrivant le contenu du champ "Description"
+  const [description, setDescription] = useState(task.description);
+
+  // Crée une fonction permettant de gérer la validation du formulaire
+  const handleSubmit: FormEventHandler = (event) => {
+    // Empêche le rechargement de la page
+    event.preventDefault();
+    // Déclenche la mise à jour de la tâche
+    updateTask(task.id, { description, done: task.done });
+    // Désactive la modification de la tâche
+    setIsBeingEdited(false);
+  }
+
+  // Rendu du composant
+  return (
+    <ListGroup.Item key={task.id} className="d-flex justify-content-between">
+      {
+        isBeingEdited ?
+          <div className="d-flex flex-grow-1">
+            <Form className="d-flex flex-grow-1" onSubmit={handleSubmit}>
+              <Form.Control
+                className="flex-grow-1"
+                type="text"
+                size="sm"
+                onChange={(event) => setDescription(event.target.value)}
+                value={description}
+              />
+              <Button variant="success" size="sm" type="submit">
+                <FaCheck />
+              </Button>
+            </Form>
+            <Button 
+              variant="secondary" 
+              size="sm"
+              onClick={() => setIsBeingEdited(false)}
+            >
+              <FaTimes />
+            </Button>
+          </div>
+        :
+          <div className="d-flex flex-grow-1">
+            <div className="flex-grow-1">
+              {task.description}
+            </div>
+            <Button
+              variant="primary"
+              size="sm"
+              onClick={() => setIsBeingEdited(true)}
+            >
+              <FaPen />
+            </Button>
+          </div>
+      }
+      <Button
+        variant="danger"
+        size="sm"
+        onClick={() => deleteTask(task.id)}
+      >
+        <FaTrashAlt />
+      </Button>
+    </ListGroup.Item>
+  )
+}
 
 function App() {
   // État permettant de stocker la liste de toutes les tâches existantes
   const [tasks, setTasks] = useState<Task[]>([]);
-  // État décrivant le contenant du champ "Description de la nouvelle tâche"
+  // État décrivant le contenu du champ "Description de la nouvelle tâche"
   const [description, setDescription] = useState('');
 
   // Associe un effet à certaines conditions
@@ -29,7 +102,7 @@ function App() {
     // Empêche le rechargement de la page
     event.preventDefault();
     // Compile les saisies de l'utilisateur dans un nouvel objet
-    const newTask = {
+    const newTask: TaskInput = {
       description,
       done: false,
     };
@@ -59,6 +132,22 @@ function App() {
     .then(response => setTasks(tasks.filter( task => task.id !== id )));
   }
 
+  // Crée une fonction permettant de modifier une tâche existante
+  const updateTask = (id: number, updatedTask: TaskInput) => {
+    // Envoie une requête HTTP à l'API permettant de modifier un enregistrement existant
+    fetch(`http://localhost:8000/tasks/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify(updatedTask),
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    // Dès que la requête a répondu, interprète le contenu de la réponse en JSON
+    .then(response => response.json())
+    // Dès que l'interprétation du contenu est terminée, remplace la tâche actuelle par sa version modifée dans la liste des tâches existantes
+    .then( (data: Task) => setTasks(tasks.map( task => task.id === id ? data : task )));
+  }
+  
   // Si la requête n'a pas encore répondu, affiche un message "Chargement…"
   if (tasks.length === 0) {
     return <div>Loading...</div>;
@@ -68,27 +157,19 @@ function App() {
   return (
     <Container>
       <h1>Liste des tâches</h1>
+
+      {/* List des tâches */}
       <ListGroup>
         {
           tasks.map(
             task => (
-              <ListGroup.Item key={task.id} className="d-flex justify-content-between">
-                <div>
-                  {task.description}
-                </div>
-                <Button
-                  variant="danger"
-                  size="sm"
-                  onClick={() => deleteTask(task.id)}
-                >
-                  <FaTrashAlt />
-                </Button>
-              </ListGroup.Item>
+              <TaskListItem task={task} deleteTask={deleteTask} updateTask={updateTask} />
             )
           )
         }
       </ListGroup>
 
+      {/* Formulaire de création de tâche */}
       <Form className="mt-4" onSubmit={handleSubmit}>
         <Form.Group>
           <Form.Label>Créer une nouvelle tâche</Form.Label>
--- src/types/api.ts
+++ src/types/api.ts
@@ -1,5 +1,12 @@
+// Représente une tâche récupérée dans l'API
 export interface Task {
   id: number
+  description: string
+  done: boolean
+}
+
+// Représente les données nécessaires à la création ou la modification d'ne tâche
+export interface TaskInput {
   description: string
   done: boolean
 }
